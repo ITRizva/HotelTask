e1ca4b7b406e10dd908d34428d70d4d7a2199abd
AmbieS <rizvanb19@gmail.com>
2023-09-01 11:55:56 +0300

    check point

--- app/src/main/java/com/example/hoteltest/presentation/hotel/HotelScreen.kt
+++ app/src/main/java/com/example/hoteltest/presentation/hotel/HotelScreen.kt
@@ -1,11 +1,13 @@
 package com.example.hoteltest.presentation.hotel
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.example.hoteltest.R
 import com.example.hoteltest.databinding.FragmentHotelScreenBinding
 
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +21,10 @@ class HotelScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHotelScreenBinding.inflate(inflater,container,false)
+        val list:List<Int>  = listOf(R.mipmap.first,R.mipmap.second,R.mipmap.third)
+        val adapter = HotelViewPagerAdapter(list)
+        binding?.viewPager2?.adapter = adapter
+        binding?.chipGroup.add
         return binding?.root
     }
 }
--- app/src/main/java/com/example/hoteltest/presentation/hotel/HotelViewPagerAdapter.kt
+++ app/src/main/java/com/example/hoteltest/presentation/hotel/HotelViewPagerAdapter.kt
@@ -1,27 +1,50 @@
 package com.example.hoteltest.presentation.hotel
 
+import android.graphics.Bitmap
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.hoteltest.databinding.FragmentHotelScreenBinding
+import com.example.hoteltest.databinding.HotelViewpagerItemBinding
 
-class HotelViewPagerAdapter(private val images:List<Int>):RecyclerView.Adapter<HotelViewPagerAdapter.ViewPagerViewHolder> (){
-    inner class ViewPagerViewHolder(private val binding:FragmentHotelScreenBinding):RecyclerView.ViewHolder(binding.root){
+class HotelViewPagerAdapter(private val images: List<Int>) :
+    RecyclerView.Adapter<ViewPagerViewHolder>() {
 
-    }
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): HotelViewPagerAdapter.ViewPagerViewHolder {
-        TODO("Not yet implemented")
+    ): ViewPagerViewHolder {
+        return ViewPagerViewHolder.createList(parent)
     }
 
-    override fun onBindViewHolder(holder: HotelViewPagerAdapter.ViewPagerViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(
+        holder: ViewPagerViewHolder,
+        position: Int
+    ) {
+        val currentImage = images[position]
+        holder.drawItem(currentImage)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return images.size
     }
+}
+
+class ViewPagerViewHolder(private val binding: HotelViewpagerItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        fun createList(list: ViewGroup): ViewPagerViewHolder = ViewPagerViewHolder(
+            HotelViewpagerItemBinding.inflate(
+                (LayoutInflater.from(list.context)),
+                list,
+                false
+            )
+        )
+    }
+    fun drawItem(image:Int){
+        binding.hotelImage.setImageResource(image)
+    }
+
 }
