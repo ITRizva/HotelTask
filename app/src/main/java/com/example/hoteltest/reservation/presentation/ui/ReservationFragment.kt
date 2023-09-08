710c199776cec8e4a9f0c00b652a3e70727f2d54
AmbieS <rizvanb19@gmail.com>
2023-09-08 12:16:24 +0300

    убрал лишнюю LiveData

--- app/src/main/java/com/example/hoteltest/reservation/presentation/ui/ReservationFragment.kt
+++ app/src/main/java/com/example/hoteltest/reservation/presentation/ui/ReservationFragment.kt
@@ -23,7 +23,7 @@ import java.io.Serializable
 
 
 @AndroidEntryPoint
-class ReservationFragment : BaseFragment<FragmentReservationFragmentBinding>() {
+class ReservationFragment : BaseFragment<FragmentReservationFragmentBinding>()  {
 
     private val viewModel by viewModels<ReservationViewModel>()
 
